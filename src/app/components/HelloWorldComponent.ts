39aa656a94caf3f72db9c7bd4ca0fa238e2bbc14
Gustavo Ferreira <70302908+Gussstavof@users.noreply.github.com>
2023-03-08 11:50:09 -0300

    start

---
+++ src/app/components/HelloWorldComponent.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'hello',
+  template: '<h1> hello world</h1>',
+  styles: ['h1 {color: blue; }']
+})
+export class HelloWorldComponent{
+
+}
